988c7295384c480a3b764d7cf79792040a345368
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2019-01-21 20:49:37 +0100

    Fanns två "app.js"
    mergade dem

--- Server.js
+++ Server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const jsonflex = require('jsonflex')();
+const Sass = require('./sass');
+const config = require('./config.json');
 const CreateRestRoutes = require('./CreateRestRoutes');
+
+for (let conf of config.sass) {
+  new Sass(conf);
+}
 
 module.exports = class Server {
 
@@ -32,6 +39,8 @@ module.exports = class Server {
     // Add body-parser to our requests
     app.use(bodyParser.json());
 
+    app.use(jsonflex);
+
     // Serve static files from www
     app.use(express.static('www'));
 
@@ -42,6 +51,31 @@ module.exports = class Server {
 
     // create all necessary rest routes for the models
     new CreateRestRoutes(app, db, models);
+
+    const fs = require('fs');
+    const path = require('path');
+
+    // Automatically load all scripts at root level of js folder
+    // and load their corresponding template files
+    app.get('/autoload-js-and-templates', (req, res) => {
+      let files = fs.readdirSync(path.join(__dirname, '/www/js'));
+      files = files.filter(x => x.substr(-3) === '.js')
+      let html = files.map(x => `<script src="/js/${x}"></script>`).join('');
+      html += files.filter(x => fs.existsSync(path.join(
+        __dirname, '/www/templates', x.split('.js').join('.html')
+      ))).map(x => `<script src="/template-to-js/${
+        x.split('.js').join('.html')}"></script>`).join('');
+      res.send(`document.write('${html}')`);
+    });
+
+    // Convert a template to a js render method
+    app.get('/template-to-js/:template', (req, res) => {
+      let html = fs.readFileSync(path.join(
+        __dirname, '/www/templates', req.params.template));
+      html = req.params.template.split('.html')[0] +
+        '.prototype.render = function(){ return `\n' + html + '\n`};'
+      res.send(html);
+    });
 
     // Start the web server
     app.listen(3000, () => console.log('Listening on port 3000'));
