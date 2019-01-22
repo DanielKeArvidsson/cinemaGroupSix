2cb9227a9c53bd4de34e6e9566822d8b05e765c1
David <David_stralfors@hotmail.com>
2019-01-22 13:27:44 +0100

    login

--- www/js/StartPage.js
+++ www/js/StartPage.js
@@ -3,5 +3,20 @@ class StartPage extends Component {
   constructor() {
     super();
     this.addRoute('/', 'Start');
+    this.addEvents({
+      'click .user-submit': 'userSubmit'
+    });
   }
+
+  userSubmit(){
+    let newUser = new User(
+      {
+      name: this.baseEl.find('#user-name').val(),
+      password: this.baseEl.find('#user-password').val()        
+      }
+    )
+    newUser.save();
+  }
+
+
 }
