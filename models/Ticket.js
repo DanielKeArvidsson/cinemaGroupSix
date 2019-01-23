948f5a1c17e3f792f919ee0a2aef12e096d018be
Mirwais Nezami <mirwais79@hotmail.com>
2019-01-23 12:17:49 +0100

    showtime, ticket
    model

---
+++ models/Ticket.js
@@ -0,0 +1,20 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+// Create a schema for ticket
+
+let ticketSchema = new Schema({
+    "showtime":{type: Schema.Types.ObjectId, ref: 'Showtime'},
+    "purchasedAt": Date,
+    "purchasedBy": {type: Schema.Types.ObjectId, ref: 'User'},
+    "isRedeemed": Boolean,
+    "movie": {type: Schema.Types.ObjectId, ref: 'User'},
+    "seat":{type: Schema.Types.ObjectId, ref: 'Seat'},
+    "auditorium": {type: Schema.Types.ObjectId, ref:'Program'}
+
+
+});
+
+
+
+module.exports = db.model('Ticket', ticketSchema);
