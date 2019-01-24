ce2ea82679ba6c3a90cb749453cb93c01ab665a9
Mirwais Nezami <mirwais79@hotmail.com>
2019-01-24 14:52:01 +0100

    small fix models

--- models/Ticket.js
+++ models/Ticket.js
@@ -4,10 +4,9 @@ const Schema = mongoose.Schema;
 // Create a schema for ticket
 
 let ticketSchema = new Schema({
-    "showtime":{type: Schema.Types.ObjectId, ref: 'Showtime'},
+    
     "purchasedAt": Date,
     "purchasedBy": {type: Schema.Types.ObjectId, ref: 'User'},
-    "isRedeemed": Boolean,
     "movie": {type: Schema.Types.ObjectId, ref: 'Movie'},
     "seat":{type: Schema.Types.ObjectId, ref: 'Seat'},
     "auditorium": {type: Schema.Types.ObjectId, ref:'Auditorium'}
