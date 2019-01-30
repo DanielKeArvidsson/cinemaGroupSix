fbd01498f7d6be863d11795bbaf234d24eefca84
Mirwais Nezami <mirwais79@hotmail.com>
2019-01-30 20:12:23 +0100

    BookTicket

---
+++ www/js/BookTicket.js
@@ -0,0 +1,43 @@
+class BookTicket extends REST {
+
+    constructor(){
+        super();
+        this.addRoute('/book-ticket', 'Boka Biljett');
+        this.addEvents({ 'click #seleckt-program': 'selectProgram'});
+        this.programs = [];
+        this.fetchPrograms();
+        this.selecktedProgram = {};
+    }
+
+    async fetchPrograms(){
+        const programs = await fetch('http://localhost:3000/json/programs',{
+            method: 'GET'
+        });
+
+        this.programs = await programs.json();
+        this.render();
+    }
+    generateProgramsList(){
+        let html = '';
+        for (let program of this.programs){
+            html += <option value="${program._id}">${program.time}</option>
+        }
+        return html
+    }
+
+    async selectProgram(){
+        const programId = this.baseEl.find('#program-select').val();
+        this.selectedProgram = await Program.find(`.findOne({ _id: '${programId}'}).populate({
+
+            path: 'auditorium',
+       
+            select: 'name seats bookings -_id',
+       
+          })
+          .populate({
+              path: 'movie',select: 'title - _id'
+          }).exec()`);
+          this.seatSeclector = new SeatSelector(this.selectedProgram);
+          this.render();
+    }
+}
--- www/js/PageContent.js
+++ www/js/PageContent.js
@@ -3,6 +3,7 @@ class PageContent extends Component {
   constructor() {
     super();
     this.startPage = new StartPage();
+    this.bookTicketPage = new BookTicketPage();
     this.missingPage = new MissingPage();
   }
 
