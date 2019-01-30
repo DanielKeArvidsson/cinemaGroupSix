519cd88985e28aed48cce7f836d2a5968ac290f7
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2019-01-30 19:26:18 +0100

    auditorium-test

--- www/js/Auditorium.js
+++ www/js/Auditorium.js
@@ -1,30 +1,7 @@
 class Auditorium extends REST {
   constructor() {
     super();
-    this.addRoute('/show-auditorium')
+    this.addBaseRoute('/show-auditorium')
   }
-  async showAuditoriumLayout() {
-    let auditorium = await Auditorium.find(`.findOne({name: /Lilla/})`);
-    auditorium = auditorium.seatsPerRow;
-    let counter = 0;
-    let i = 0;
-    let holder = [];
-    let rowCounter = auditorium[counter];
-    let counterBackward = auditorium[counter]
-    for (let obj in auditorium) {
-      while (i < auditorium[counter]) {
-        holder.push(counterBackward + " ")
-        counterBackward--
-        i++
-      };
-      $(".bio").append("<p>" + holder.toString() + "</p>")
-      $(".bio").append("<br>");
-      holder = [];
-      counter++
-      rowCounter += auditorium[counter];
-      counterBackward = rowCounter
-      i = 0
-    };
-  };
-  showAuditoriumLayout();
+
 }
--- www/js/test.js
+++ www/js/test.js
@@ -1,5 +1,24 @@
-async function test() {
-  let movies = await Movie.find();
-  console.log(movies);
-}
-test();
+async function showAuditoriumLayout() {
+  let auditorium = await Auditorium.find(`.findOne({name: /Lilla/})`);
+  auditorium = auditorium.seatsPerRow;
+  let counter = 0;
+  let i = 0;
+  let holder = [];
+  let rowCounter = auditorium[counter];
+  let counterBackward = auditorium[counter]
+  for (let obj in auditorium) {
+    while (i < auditorium[counter]) {
+      holder.push(counterBackward + " ")
+      counterBackward--
+      i++
+    };
+    $(".bio").append("<p>" + holder.toString() + "</p>")
+    $(".bio").append("<br>");
+    holder = [];
+    counter++
+    rowCounter += auditorium[counter];
+    counterBackward = rowCounter
+    i = 0
+  };
+};
+showAuditoriumLayout()
