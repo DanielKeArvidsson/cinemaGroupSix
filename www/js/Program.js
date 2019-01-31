514aaea743fc9fdb224a4c8c3e11632cf85719e4
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2019-01-31 15:09:45 +0100

    SUperfix by Thomas!

--- www/js/Program.js
+++ www/js/Program.js
@@ -1,11 +1,26 @@
 class Program extends Component {
-  constructor() {
-    super();
+  constructor(props) {
+    super(props);
+
+    // If my property movie is populated then convert it to a real instance of Movie
+    if (typeof this.movie !== "string") {
+      // it is populated so convert to a real instance of Movie
+      this.movie = new Movie(this.movie);
+    }
+
+    // If my property auditorium is populated then convert it to a real instance of Auditorium
+    if (typeof this.auditorium !== "string") {
+      // it is populated so convert to a real instance of Auditorium
+      this.auditorium = new Auditorium(this.auditorium);
+    }
+
+
+
+
+
+
+
     this.addRoute('/prog', 'Prog');
-    this.asdf();
   }
-  async asdf() {
-    let programs = await Program.find()
-    console.log(programs[0].movie);
-  }
+
 }
