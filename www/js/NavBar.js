83f5b9547c3566faddbcc6df8546f4eac2e094d3
Mirwais Nezami <mirwais79@hotmail.com>
2019-01-31 18:15:02 +0100

    Link program page

--- www/js/NavBar.js
+++ www/js/NavBar.js
@@ -4,6 +4,7 @@ class NavBar extends Component {
     super();
     this.navItems = [
       new NavItem('Start', '/'),
+      new NavItem('Boka Visning', '/book-ticket'),
       new NavItem('About', '/about')
     ];
   }
