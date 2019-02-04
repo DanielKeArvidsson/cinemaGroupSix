54a249843200019f7b21077aaa207263dc8b3aac
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2019-02-04 14:33:07 +0100

    show movies

--- www/js/PageContent.js
+++ www/js/PageContent.js
@@ -2,11 +2,12 @@ class PageContent extends Component {
 
   constructor() {
     super();
+    this.movies = []; // will be changed from App
     this.startPage = new StartPage();
     this.bookTicket = new BookTicket();
     this.salong = new Salong();
     this.loginPage = new LoginPage();
-    this.missingPage = new MissingPage();    
+    this.missingPage = new MissingPage();
   }
 
 }
