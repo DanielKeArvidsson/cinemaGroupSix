840cc5b204a0bf093623ceb12f6f2464c9c689e1
Kim <kim.johnsson@telia.com>
2019-02-07 12:45:02 +0100

    selected program

--- www/js/BookTicketPage.js
+++ www/js/BookTicketPage.js
@@ -1,24 +1,26 @@
 class BookTicketPage extends Component {
-  constructor(program) {
+  constructor() {
     super();
     this.addRoute('/book-ticket', 'Book Ticket');
-    this.program = program;
-    console.log(this.program);
     this.addEvents({
       
-    });
-    this.selectedProgram = {};
-
-    this.salong = new Salong(this.program);
-    this.salong.getSalong('Stora Salongen')
-  
+    });  
   }
   
-  mount() {
+  async mount() {
+    this.salong = new Salong();
     this.program = App.programId;
-    console.log(this.program);
+    this.selectedProgram = await Program.find(this.program);
+    this.seating = this.salong.getSalong(this.selectedProgram.auditorium.name);
+    console.log(this.selectedProgram)
     this.render();
   }
- 
+
+  unmount(){
+
+    delete this.salong
+    console.log('unmount')
+
+  }
   
 }
