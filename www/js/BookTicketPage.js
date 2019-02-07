8235b842e70fd5fc097568f98598029f04ec8021
Tommy <tommynguyen@live.se>
2019-02-07 12:15:56 +0100

    fixed whit bookseats

--- www/js/BookTicketPage.js
+++ www/js/BookTicketPage.js
@@ -1,24 +1,31 @@
 class BookTicketPage extends Component {
-  constructor(program) {
+  constructor(program, seat) {
     super();
     this.addRoute('/book-ticket', 'Book Ticket');
+    this.seat = seat;
     this.program = program;
     console.log(this.program);
     this.addEvents({
-      
+      'click .booked-tickets': 'bookSeat'
     });
     this.selectedProgram = {};
 
     this.salong = new Salong(this.program);
     this.salong.getSalong('Stora Salongen')
-  
+
   }
-  
+
   mount() {
     this.program = App.programId;
     console.log(this.program);
     this.render();
   }
- 
-  
+
+  bookSeat() {
+    let elements = document.getElementsByClassName('choosenSeat');
+    for (let i = elements.length - 1; i >= 0; --i) {
+      elements[i].className = 'unavailableSeat';
+    }
+  }
+
 }
