07a53863d51d641b1c3b8b87c1de5bef646bcac0
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2019-02-11 12:18:18 +0100

    merge dev in booking-test

--- www/js/BookTicketPage.js
+++ www/js/BookTicketPage.js
@@ -1,7 +1,8 @@
 class BookTicketPage extends Component {
-  constructor(seat,props) {
+  constructor(seat, props) {
     super(props);
     this._props = props
+    this.seat = seat;
     this.addRoute(/\/program\/(.*)/, 'Visning')
     this.addEvents({
       'click .booked-tickets': 'bookSeat'
@@ -9,15 +10,16 @@ class BookTicketPage extends Component {
   }
   async mount() {
     let id = this.routeParts[0];
-    this.salong = await new Salong();
+    this.salong = new Salong();
     let program = await Program.find(`.findById('${id}').populate('movie auditorium').exec()`);
     this.salongen = await this.salong.getSalong(program.auditorium.name);
     document.title = 'Program: ' + program.movie.title;
     Object.assign(this, program._props);
     this.render();
   }
-
-
+  unmount() {
+    delete this.salong;
+  }
   bookSeat() {
     let elements = document.getElementsByClassName('choosenSeat');
     for (let i = elements.length - 1; i >= 0; --i) {
