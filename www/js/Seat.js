87b5cf39bbec6d0d46b13cce5c39850f255fdd3c
Tommy <tommynguyen@live.se>
2019-02-19 15:01:34 +0100

    Booking page fixes

--- www/js/Seat.js
+++ www/js/Seat.js
@@ -1,43 +1,50 @@
-class Seat extends Component{
-    constructor(seats, rows, props){
-        super(props);
-        this.seatNumber = seats;
-        this.rowNumber = rows;
-        this.choosenSeats = 0;
-        this.addEvents({
-            'click': 'getSeat',
-        });
-    }
+class Seat extends Component {
+  constructor(seats, rows, props) {
+    super(props);
+    this.seatNumber = seats;
+    this.rowNumber = rows;
+    this.choosenSeats = 0;
+    this.addEvents({
+      'click': 'getSeat',
+      'mouseenter': 'hover'
+    });
+  }
 
-    async getSeat(){
-        this.test = []
+  async getSeat() {
 
+    for (let valdaPlatser of this.test2) {
+        valdaPlatser.baseEl[0].className = 'hoverChoosenSeat'
+      }
 
-        if(this.baseEl[0].className == 'choosenSeat'){
-            this.baseEl[0].className = 'seat';
-        }
+  }
 
-        if(App.bookTicketsPage.totalTickets > this.choosenSeats){
-            this.baseEl[0].className = 'choosenSeat'
-        }
+  hover(){
 
-        for (let rows = 0; rows < App.bookTicketsPage.salong.salong.length; rows++) {
-            let seats = App.bookTicketsPage.salong.salong[rows].row[0]
-            for (let seat = 0; seat < seats.length; seat++) {
-                if(seats[seat].baseEl[0].className == 'choosenSeat'){
-                    console.log(seat)
-                    this.test.push(seats[seat])
-                    for(let i = 1; i < App.bookTicketsPage.totalTickets; i++){
-                        this.test.push(seats[seat += 1])
-                    }
-                }
+    this.test2 = []
+
+    if (App.bookTicketsPage.totalTickets > this.choosenSeats) {
+        this.baseEl[0].className = 'choosenSeat'
+      }
+
+      for (let rows = 0; rows < App.bookTicketsPage.salong.salong.length; rows++) {
+        let seats = App.bookTicketsPage.salong.salong[rows].row[0]
+        for (let seat = 0; seat < seats.length; seat++) {
+          if (seats[seat].baseEl[0].className == 'choosenSeat') {
+            this.test2.push(seats[seat])
+            for (let i = 1; i < App.bookTicketsPage.totalTickets; i++) {
+              this.test2.push(seats[seat += 1])
             }
+          }
         }
+      }
 
-        console.log(this.test)
-
-        for(let valdaPlatser of this.test){
+      for (let valdaPlatser of this.test2) {
+        if(valdaPlatser.baseEl[0].className == 'hoverChoosenSeat'){
+        }else{
             valdaPlatser.baseEl[0].className = 'choosenSeat'
         }
+      }
+      
+      App.bookTicketsPage.getBookedSeats()
     }
-}
+  }
