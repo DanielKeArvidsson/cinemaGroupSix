03733e6d8f6bba8f61c24cb5646071195dcb1e12
Aleksandra <a.toholjevic@gmail.com>
2019-02-19 23:35:08 +0100

    fsfsf

--- www/js/BookingHistory.js
+++ www/js/BookingHistory.js
@@ -3,50 +3,42 @@ class BookingHistory extends Component {
     super(props);
     this.addRoute('/mina-bokningar', 'Mina bokningar');
     this.tickets = [];
-    this.message = "";
     this.show = false;
+    this.noBookings = false;
     //this.generateBookingHistory();  
     this.addEvents({
-    'click .getBook': 'generateBookingHistory',
-  
+      //  'click .getBook': 'generateBookingHistory',
+
     });
   }
 
+  mount() {
+    this.tickets = [];
+    this.customerBookings = [];
+    this.generateBookingHistory();
+    //this.render();
+  }
+
   async generateBookingHistory() {
-  
-    this.tickets.length = 0;
-    
     this.show = true;
+    this.noBookings = false;
     let tmpTickets = [];
     tmpTickets = await Ticket.find();
     let tmpEmail = await Login.find();
-    for(let i = 0;i<tmpTickets.length;i++) {
-      if (tmpTickets[i].user === tmpEmail.email){
+    for (let i = 0; i < tmpTickets.length; i++) {
+      if (tmpTickets[i].user === tmpEmail.email) {
         this.tickets.push(tmpTickets[i]);
       }
     }
     if (this.tickets.length === 0) {
-      this.message += `
-      <div class="card text-dark mb-2"">
-        <div class="card-body">
-          <h5 class="card-title">Din bokningshistoria är tom</h5>
-        </div>
-        <a class="btn btn-primary" href="/">Stäng</a>
-      </div>
-      `;
+      this.show = false;
+      this.noBookings = true;
+
     }
-
-    // console.log(App.loginPage.currentUser)
-    // this.findUser = await User.find(`.find({email: '${App.loginPage.currentUser.email}'})`);
-    // console.log(this.findUser)
-
-   // this.tickets = await Ticket.find(`.find({user: '${this.findUser._id}'}).populate('program user').exec()`);
-    // this.tickets = await Ticket.find(`.find().populate('program user').exec()`);
-     console.log(this.tickets);
-  //  console.log(tmpTickets);
+    console.log(this.tickets);
     this.render();
-
   }
+
 
 
 }
--- www/js/LoginPage.js
+++ www/js/LoginPage.js
@@ -6,8 +6,9 @@ class LoginPage extends Component {
     this.currentLogin = "";
     this.currentUserInfo = "";
     this.showModal = false;
-    this.showLogin = true;
+    this.hideLogin = false;
     this.showError = false;
+    this.showErrorPassword = false;
     this.showYouAreLoggedIn = false;
     this.addRoute('/login', 'Login');
     this.addEvents({
@@ -31,47 +32,103 @@ class LoginPage extends Component {
     )
     let user = await User.find(`.find({email: '${newLogin.email}'})`);
     this.currentUserInfo = user[0];
-    console.log(this.currentUserInfo);
+    //console.log(this.currentUserInfo);
+    //console.log(user);
 
-    console.log(user);
-    if (user.length === 0) {
-      this.showLogin = false;
-      this.showError = true;
-      console.log("error: 'No such user!'");
 
-      //   $('.login-form').empty();
-      //   $('.login-form').append(`
-      //   <div>
-      //  <div id="myModal" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false">
-      //     <div class="modal-dialog" role="document">
-      //       <div class="modal-content">
-      //         <div class="modal-header">
-      //           <h5 class="modal-title">No such user!</h5>
-      //         </div>
-      //         <div class="modal-body">
-      //           <p>Please register</p>
-      //         </div>
-      //         <div class="modal-footer">
-      //          <a href="/register">Stäng</a>
-      //         </div>
-      //       </div>
-      //     </div>
-      //   </div>
-      // </div>
-      // `)
-    } else {
+
+    let result = await newLogin.save();
+    console.log(result.loggedIn);
+
+    console.log(result, "rsult");
+    let test = await Login.find();
+    console.log(test);
+    if (result.error && result.error == "The password does not match!") {
+      console.log("errorrrr");
+      this.hideLogin = true;
+      this.showErrorPassword = true;
+      //     $('.login-form').empty();
+      //    $('.login-form').append(`
+      //    <div>
+      //   <div id="myModal" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false">
+      //      <div class="modal-dialog" role="document">
+      //        <div class="modal-content">
+      //          <div class="modal-header">
+      //            <h5 class="modal-title">Password doesnt match</h5>
+      //          </div>
+      //          <div class="modal-body">
+      //            <p>Please register</p>
+      //          </div>
+      //          <div class="modal-footer">
+      //           <a href="/login">Stäng</a>
+      //          </div>
+      //        </div>
+      //      </div>
+      //    </div>
+      //  </div>
+      //  `)     
+    }
+    else if (result.error && result.error === "No such user!") {
+      console.log("errorrrr");
+      $('.login-form').empty();
+      $('.login-form').append(`
+      <div>
+     <div id="myModal" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false">
+        <div class="modal-dialog" role="document">
+          <div class="modal-content">
+            <div class="modal-header">
+              <h5 class="modal-title">No such user</h5>
+            </div>
+            <div class="modal-body">
+              <p>Please register</p>
+            </div>
+            <div class="modal-footer">
+             <a href="/register">Stäng</a>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+    `)
+    }
+    // else if (user.length === 0) {
+    //   this.showLogin = false;
+    //   this.showError = true;
+    //   console.log("error: 'No such user!'");
+
+    //   //   $('.login-form').empty();
+    //   //   $('.login-form').append(`
+    //   //   <div>
+    //   //  <div id="myModal" tabindex="-1" role="dialog" data-backdrop="static" data-keyboard="false">
+    //   //     <div class="modal-dialog" role="document">
+    //   //       <div class="modal-content">
+    //   //         <div class="modal-header">
+    //   //           <h5 class="modal-title">No such user!</h5>
+    //   //         </div>
+    //   //         <div class="modal-body">
+    //   //           <p>Please register</p>
+    //   //         </div>
+    //   //         <div class="modal-footer">
+    //   //          <a href="/register">Stäng</a>
+    //   //         </div>
+    //   //       </div>
+    //   //     </div>
+    //   //   </div>
+    //   // </div>
+    //   // `)
+    else if (result.loggedIn === true) {
       Store.loggedIn = true;
       Store.userIsLoggedIn = true;
       console.log(Store.loggedIn, "store");
       this.showLogin = false;
       this.showYouAreLoggedIn = true;
       console.log("Successfully logged in");
-      console.log(User.loggedIn);
-      Store.currentUser =  user[0].firstName;
+
+      Store.currentUser = user[0].firstName;
       console.log("finallyyyyy", Store.currentUser);
-      newLogin.save();
+      //   newLogin.save();
       this.currentUser = newLogin;
-      console.log(Login.loggedIn);
+
 
       //   $('.login-form').empty();
       //   $('.login-form').append(`
@@ -93,12 +150,13 @@ class LoginPage extends Component {
       //   </div>
       // </div>
       // `)
+
+
+      Store.navbar.render();
+      this.render();
+      console.log("RERENDERING!");
+      console.log(this.currentUser);
     }
-   
-    Store.navbar.render();
-    this.render();
-    console.log("RERENDERING!");
-    console.log(this.currentUser);
   }
 
 
@@ -138,18 +196,22 @@ class LoginPage extends Component {
     let toDeleteUser = await Login.find();
     this.currentLogin = await Login.find();
     console.log(toDeleteUser, "to be deleted");
-    console.log(this.currentLogin,"current");
+    console.log(this.currentLogin, "current");
     await toDeleteUser.delete();
- //   await this.currentUser.delete();
-  Store.loggedIn = false;
-  Store.userIsLoggedIn = false;
+    //   await this.currentUser.delete();
+    Store.loggedIn = false;
+    Store.userIsLoggedIn = false;
     Store.navbar.userIsLoggedIn = false;
-    
+
     Store.navbar.render();
-    
+
     this.render();
   }
 
+
+  // validate(){
+  //   bghgg
+  // }
 
 
 }
