96a559837ecb474cdb1aed7c09d868294b4f4796
Tommy <tommynguyen@live.se>
2019-02-19 11:20:31 +0100

    stuff

--- www/js/Seat.js
+++ www/js/Seat.js
@@ -6,15 +6,40 @@ class Seat extends Component{
         this.addEvents({
             'click': 'getSeat',
         });
+        this.addEvents({
+            'mouseenter': 'selectMultipleSeats',
+        });
     }
 
     async getSeat(){
-
         if(this.baseEl[0].className == 'choosenSeat'){
             this.baseEl[0].className = 'seat';
         }else{
             this.baseEl[0].className = 'choosenSeat'
         }
+    }
+
+    async selectMultipleSeats(){
+        /*
+            Need to access how many seats that the customer have selected. 
+            To identify which seats should be highlighted.
+
+            Example:
+                [1] [2] [3] [4]
+                [5] [6] [7] [8]
+
+            If seat 1 is "HOVERED" and customer have selected 3 seats then 1,2 and 3 should be highlighted.
+            By knowing "this.totalTickets" from BookingTicketPage.js it should be possible to know which seats that has to be highlighted.
+
+            If you can't access "this.totalTickets" from BookingTicketPage.js you could query the DOM and pick the seats from there and sum the total 
+            tickets..
+
+
+
+
+        */
+
+        console.log("hover!", this.totalTickets)
     }
 
 }
