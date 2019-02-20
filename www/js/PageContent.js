977b97603a4dc2ebd6f560ae3c4bef79f5013e89
Kim <kim.johnsson@telia.com>
2019-02-20 12:32:47 +0100

    fix

--- www/js/PageContent.js
+++ www/js/PageContent.js
@@ -9,8 +9,7 @@ class PageContent extends Component {
     this.bookTicketPage = new BookTicketPage();
     App.bookTicketsPage = this.bookTicketPage;
     this.loginPage = new LoginPage();
-    App.loginPage = this.loginPage;
-    this.bookTicketPage = new BookTicketPage();  
+    App.loginPage = this.loginPage;  
     this.registerPage = new RegisterPage();
     this.bookingHistory = new BookingHistory();
     this.salongsInfoPage = new SalongsInfoPage();
