68aa9b619d5e9838fc8de9b03e0462a721470844
Mirwais Nezami <mirwais79@hotmail.com>
2019-03-04 19:29:05 +0100

    merge fix

--- src/App.js
+++ src/App.js
@@ -1,25 +1,25 @@
 import React, { Component } from "react";
 import "./css/style.css";
 import ShowPage from "./components/ShowPage";
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
 import NavBar from './components/NavBar';
 import StartPage from './components/StartPage';
 import Footer from './components/Footer';
 // import './css/style.css';
-import { Route } from 'react-router-dom'
+// import { Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <ShowPage />
       
       <header>
       <NavBar />
       </header>
       
       <main>
-      <Route exact path="/" components={StartPage} />
+      <ShowPage />
+      <StartPage />
       </main>
 
       <footer>
