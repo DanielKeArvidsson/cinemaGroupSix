f8412ab1884a38e3c1ae153c2997a6db312aa033
Mirwais Nezami <mirwais79@hotmail.com>
2019-03-04 13:11:17 +0100

    Navbar

--- src/App.js
+++ src/App.js
@@ -1,25 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
-import './css/style.css';
+import NavBar from './components/NavBar';
+import StartPage from './components/StartPage';
+// import './css/style.css';
+import { Route } from 'react-router-dom'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <header>
+      <NavBar />
+      </header>
+      <main>
+      <Route exact path="/" components={StartPage} />
+      </main>
+      <footer>
+      
+      
+      </footer>
       </div>
     );
   }
---
+++ src/components/NavBar.jsx
@@ -0,0 +1,63 @@
+import React, { Component } from 'react';
+import {
+  Container,
+  Navbar,
+  NavbarToggler,
+  Collapse,
+  Nav,
+  NavItem,
+} from 'reactstrap'
+import { NavLink, Link} from 'react-router-dom'
+
+
+class NavBar extends Component {
+  constructor() {
+    super();
+    this.state = {
+    collapsed: true
+    };
+}
+
+toggleNavbar = () => {
+    this.setState({
+    collapsed: !this.state.collapsed
+    });
+}
+
+closeNavbar = () => {
+    if (!this.state.collapsed === true) {
+    this.toggleNavbar();
+    }
+}
+
+  render() {
+    return (
+      <Container>
+        <Navbar expand="lg" dark>
+          <Link to='/' className="navbar-brand">FilmVisarna AB</Link>
+          <NavbarToggler onClick={this.toggleNavbar} />
+          <Collapse isOpen={!this.state.collapsed} navbar>
+            <Nav className="ml-auto" navbar>
+              <NavItem>
+                <NavLink exact to='/' onClick={this.closeNavbar} className="nav-link">Start</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/program' onClick={this.closeNavbar} className="nav-link">Aktuella Visningar</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/salong' onClick={this.closeNavbar} className="nav-link">Våra Salonger</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/aboutUs' onClick={this.closeNavbar} className="nav-link">Om Oss</NavLink>
+              </NavItem>
+              <NavItem>
+                <NavLink to='/login' onClick={this.closeNavbar} className="nav-link">Logga in</NavLink>
+              </NavItem>
+            </Nav>
+          </Collapse>
+        </Navbar>
+      </Container>
+    )
+  }
+}
+export default NavBar;
