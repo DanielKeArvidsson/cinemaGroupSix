b33631af582947bebf5d94b4f770c3f0a80e5a44
daniel arvidsson <daniel.ke.arvidsson@hotmail.com>
2019-03-05 20:32:32 +0100

    Ändrade sökväg för sasskompilaton

--- src/App.js
+++ src/App.js
@@ -1,35 +1,27 @@
 import React, { Component } from "react";
 import "./css/style.css";
 import ShowPage from "./components/ShowPage";
-import NavBar from './components/NavBar';
-import Home from './components/home';
-import Footer from './components/footer';
-import { Route } from 'react-router-dom';
-
-
+import NavBar from "./components/NavBar";
+import Home from "./components/home";
+import Footer from "./components/footer";
+import { Route } from "react-router-dom";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-      
-      
-      <header>
-        <NavBar />
-      </header>
-      
-    
-       <main>
-         <Route exact path="/home" components={Home} />
-         <Route path="/showpage" components={ShowPage} />
-       </main>
-     
-      
-      
-      <footer>
-        <Footer />
-      </footer>
-      
+        <header>
+          <NavBar />
+        </header>
+
+        <main>
+          <Route exact path="/home" components={Home} />
+          <Route path="/showpage" components={ShowPage} />
+        </main>
+
+        <footer>
+          <Footer />
+        </footer>
       </div>
     );
   }
