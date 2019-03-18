f22d2d52875bcb8307f9828893e9c546509ca69c
Aleksandra <a.toholjevic@gmail.com>
2019-03-18 21:29:23 +0100

    added things

--- src/components/Topplista.jsx
+++ src/components/Topplista.jsx
@@ -1,26 +1,60 @@
 import React, { Component } from "react";
 import REST from "../REST";
+import MovieInfo from "./MovieInfo";
+class Movie extends REST {}
 class Ticket extends REST {}
-class Program extends REST {}
 
 export class Topplista extends Component {
   constructor(props) {
     super(props);
+    this.allBookings = [];
+    this.allMovies = [];
+    this.loadMovies();
     this.findMovies();
+
     this.state = {
       data: []
     };
-    this.allBookings = [];
+  }
+
+  async loadMovies() {
+    let movies = await Movie.find();
+    for (let movie of movies) {
+      this.allMovies.push(movie.title);
+      //     console.log(ticket.program.movie.title);
+      //     this.allBookings.push(ticket.program.movie.title);
+      //   }
+    }
+    //console.log(this.allMovies);
+  }
+
+  countMovies() {
+    let array = [];
+    for (let movie of this.allMovies) {
+      let count = 0;
+      for (let booking of this.allBookings) {
+        if (booking === movie) {
+          count++;
+        }
+      }
+      array.push({ movie, count });
+    }
+
+    array.sort(function(a, b) {
+      return a.count - b.count;
+    });
+    console.log(array);
   }
 
   async findMovies() {
     let data = await Ticket.find();
-    console.log(data);
+    // console.log(data);
     for (let ticket of data) {
-      console.log(ticket.program.movie.title);
+      //console.log(ticket.program.movie.title);
       this.allBookings.push(ticket.program.movie.title);
     }
-    console.log(this.allBookings);
+    //console.log(this.allBookings);
+    this.countMovies();
   }
 
   render() {
